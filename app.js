374d8b25c1635e7a6c9746241e979edabfa5a797
Jay king <abeltensor@gmail.com>
2016-10-05 14:01:38 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var ul = document.getElementById('list'),
+    removeAll = document.getElementById('removeAll'),
+    add = document.getElementById('add')
+
+add.onclick = function(){
+  addLi(ul);
+}
+
+function addLi(targetUl){
+  var inputText = document.getElementById('text').value,
+      li = document.createElement('li'),
+      textNode = document.createTextNode(inputText + ' '),
+      removeButton = document.createElement('button');
+  document.getElementById('text').value = '';
+  
+  if(inputText.split(' ').join('').length === 0){
+    alert('no input');
+    return false;
+  }
+  
+  removeButton.className = 'removeMe';
+  removeButton.innerHTML = ' DONE!';
+  removeButton.setAttribute('onclick', 'removeMe(this);');
+  
+  li.appendChild(textNode);
+  li.appendChild(removeButton);
+  
+  targetUl.appendChild(li);
+};
+
+function removeMe(item){
+  var parent = item.parentElement;
+  parent.parentElement.removeChild(parent);
+};
+
+removeAll.onclick = function (){
+  ul.innerHTML = '';
+};
