22af5c5fd50a8ea74789dfa6fe3a7164be042473
Jay king <abeltensor@gmail.com>
2016-10-18 22:38:19 -0400

    added server

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
 var ul = document.getElementById('list'),
-    removeAll = document.getElementById('removeAll'),
-    add = document.getElementById('add')
+  removeAll = document.getElementById('removeAll'),
+  add = document.getElementById('add')
 
-add.onclick = function(){
+add.onclick = () => {
   addLi(ul);
 }
 
-function addLi(targetUl){
+function addLi(targetUl) {
   var inputText = document.getElementById('text').value,
-      li = document.createElement('li'),
-      textNode = document.createTextNode(inputText + ' '),
-      removeButton = document.createElement('button');
+    li = document.createElement('li'),
+    textNode = document.createTextNode(inputText + ' '),
+    removeButton = document.createElement('button');
   document.getElementById('text').value = '';
-  
-  if(inputText.split(' ').join('').length === 0){
+
+  if (inputText.split(' ').join('').length === 0) {
     alert('no input');
     return false;
   }
-  
+
   removeButton.className = 'removeMe';
   removeButton.innerHTML = ' DONE!';
   removeButton.setAttribute('onclick', 'removeMe(this);');
-  
+
   li.appendChild(textNode);
   li.appendChild(removeButton);
-  
-  targetUl.appendChild(li);
-};
 
-function removeMe(item){
+  targetUl.appendChild(li);
+}
+;
+
+function removeMe(item) {
   var parent = item.parentElement;
   parent.parentElement.removeChild(parent);
-};
+}
+;
 
-removeAll.onclick = function (){
+removeAll.onclick = function() {
   ul.innerHTML = '';
 };
